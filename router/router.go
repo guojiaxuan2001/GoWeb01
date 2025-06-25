4913b800a5559ccdf1af8ac450d7dee4e03243b6
Kelly Guo <guojiaxuan2001@gmail.com>
2025-06-25 13:46:31 +0900

    Updated routher & utils

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"goweb01/controllers"
+	"goweb01/middlewares"
 )
 
 func SetupRouter() *gin.Engine {
@@ -12,6 +13,13 @@ func SetupRouter() *gin.Engine {
 	{
 		auth.POST("/login", controllers.Login)
 		auth.POST("/register", controllers.Register)
+	}
+
+	api := r.Group("/api")
+	api.GET("/exchangeRates", controllers.GetExchangeRates)
+	api.Use(middlewares.AuthMiddleware())
+	{
+		api.POST("/exchangeRates", controllers.CreateExchangeRates)
 	}
 	return r
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 	"time"
@@ -25,4 +26,30 @@ func GenerateJWT(username string) (string, error) {
 func CheckPassword(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func ParseJWT(tokenString string) (string, error) {
+	if len(tokenString) > 7 && tokenString[:7] == "Bearer " {
+		tokenString = tokenString[7:]
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok { //保证是否是正确的迁移方法
+			return nil, errors.New("unexpected Signing Method")
+		}
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username, ok := claims["username"].(string)
+		if !ok {
+			return "", errors.New("invalid token")
+		}
+		return username, nil
+	}
+	return "", err
 }
