46d228ba08fa3bfee3d3f13aaafbdc31253cd791
Kelly Guo <guojiaxuan2001@gmail.com>
2025-06-27 01:42:34 +0900

    App Completed

--- BackEnd/main.go
+++ BackEnd/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"goweb01/config"
 	"goweb01/router"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 //gin.H的源代码：
@@ -19,5 +25,26 @@ func main() {
 		port = ":8080"
 	}
 
-	r.Run(port)
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
